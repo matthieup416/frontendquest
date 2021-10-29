8e2e37c68ae5ff51a71150b5747a00484b469cd1
Gato18 <g.canonge@gmail.com>
2021-10-29 13:06:21 +0200

    result screen

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -11,13 +11,17 @@ let deviceWidth = Dimensions.get("window").width;
 
 function ResultsScreen(props) {
   const [listOffer, setListOffer] = useState([]);
+  const [quest, setQuest] = useState([]);
 
   //Au chargement du composant, on cherche toutes les offres de la quête choisie
   useEffect(() => {
     async function offers() {
       const data = await fetch(`http://${MY_IP}:3000/results?token=${props.dataUser.token}&quest_id=${"617905dba41305d7eec0d35b"}`);
       const body = await data.json();
-      console.log("body", body);
+      setListOffer(body.listOffers);
+      console.log("body.listOffers", body.listOffers);
+
+      setQuest(body.quest);
     }
     offers();
   }, []);
@@ -26,47 +30,33 @@ function ResultsScreen(props) {
     <View style={{ backgroundColor: "#FFFFFF", height: "100%" }}>
       <StatusBar backgroundColor={"#2D98DA"} style="light" />
       <ScrollView>
-        <Image source={require("../assets/maison-1.jpg")} style={{ width: "100%", height: deviceHeight / 2.8 }} resizeMethod="resize" resizeMode="center"></Image>
-        <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <View style={{ marginLeft: 20, marginRight: 10 }}>
-            <Text style={{ fontSize: 22, fontWeight: "bold", color: "#585858" }}>Maison 4 pièces 75 m2</Text>
-            <Text style={{ fontSize: 22, fontWeight: "bold", color: "#2D98DA" }}>165 000 €</Text>
-            <Text style={{ fontSize: 15, color: "#585858" }}>Caen</Text>
-          </View>
-          <View style={{ flexDirection: "column", alignItems: "flex-end", marginRight: 20, marginTop: 10 }}>
-            <Icon name="meteor" size={20} color="#FBC531" style={{ marginRight: 5 }} />
-            <Badge status="primary" value="PRO" />
-          </View>
-        </View>
-
-        <Image source={require("../assets/maison-1.jpg")} style={{ width: "100%", height: deviceHeight / 2.8 }} resizeMethod="resize" resizeMode="center"></Image>
-        <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <View style={{ marginLeft: 20, marginRight: 10 }}>
-            <Text style={{ fontSize: 22, fontWeight: "bold", color: "#585858" }}>Maison 4 pièces 75 m2</Text>
-            <Text style={{ fontSize: 22, fontWeight: "bold", color: "#2D98DA" }}>165 000 €</Text>
-            <Text style={{ fontSize: 15, color: "#585858" }}>Caen</Text>
-          </View>
-          <View style={{ flexDirection: "column", alignItems: "flex-end", marginRight: 20, marginTop: 10 }}>
-            <Icon name="meteor" size={20} color="#FBC531" style={{ marginRight: 5 }} />
-            <Badge status="primary" value="PRO" containerStyle={{}} />
-          </View>
-        </View>
-
-        <Image source={require("../assets/maison-1.jpg")} style={{ width: "100%", height: deviceHeight / 2.8 }} resizeMethod="resize" resizeMode="center"></Image>
-        <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <View style={{ marginLeft: 20, marginRight: 10 }}>
-            <Text style={{ fontSize: 22, fontWeight: "bold", color: "#585858" }}>Maison 4 pièces 75 m2</Text>
-            <Text style={{ fontSize: 22, fontWeight: "bold", color: "#2D98DA" }}>165 000 €</Text>
-            <Text style={{ fontSize: 15, color: "#585858" }}>Caen</Text>
-          </View>
-          <View style={{ flexDirection: "column", alignItems: "flex-end", marginRight: 20, marginTop: 10 }}>
-            <Icon name="meteor" size={20} color="#FBC531" style={{ marginRight: 5 }} />
-            <Badge status="primary" value="PRO" containerStyle={{}} />
-          </View>
-        </View>
+        {listOffer.map((offer, i) => {
+          if (offer.is_pro) {
+            var pro = <Badge status="primary" value="PRO" />;
+          }
+          if (new Date(offer.offers.created) > new Date(new Date().setDate(new Date().getDate() - 1))) {
+            var meteor = <Icon name="meteor" size={20} color="#FBC531" style={{ marginRight: 5, marginBottom: 5 }} />;
+          }
+          return (
+            <TouchableOpacity key={i}>
+              <Image source={require("../assets/maison-1.jpg")} style={{ width: "100%", height: deviceHeight / 2.8 }} resizeMethod="resize" resizeMode="center"></Image>
+              <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
+                <View style={{ marginLeft: 20, marginRight: 10 }}>
+                  <Text style={{ fontSize: 22, fontWeight: "bold", color: "#585858" }}>
+                    {offer.offers.type} {offer.offers.nb_pieces} pièces {offer.offers.surface} m2
+                  </Text>
+                  <Text style={{ fontSize: 22, fontWeight: "bold", color: "#2D98DA" }}>{offer.offers.price} €</Text>
+                  <Text style={{ fontSize: 15, color: "#585858" }}>{offer.offers.city}</Text>
+                </View>
+                <View style={{ flexDirection: "column", alignItems: "flex-end", marginRight: 20, marginTop: 10 }}>
+                  {meteor}
+                  {pro}
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
       </ScrollView>
-
-      <Text>Résultats des offres pour une quête</Text>
     </View>
   );
 }
