98e361f2c4e6ef0b6d85dfdafab41d4540d1834d
Gato18 <g.canonge@gmail.com>
2021-11-02 18:58:55 +0100

    flashMessage et edit screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { View, Text, StyleSheet, Dimensions, TextInput, ScrollView } from "react-native";
 import { Avatar } from "react-native-elements";
 import { connect } from "react-redux";
 import CreatButton from "../shared/CreatButton";
+import { showMessage, hideMessage } from "react-native-flash-message";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -10,16 +11,20 @@ import { MY_IP } from "@env"; /* Variable environnement */
 
 let screenWidth = Dimensions.get("window").width;
 
-
 function ProfileScreen(props) {
   var handleSubmitRemove = async () => {
     console.log("Déconnecté!");
     AsyncStorage.removeItem("token");
     props.navigation.navigate("SignIn", { screen: "SignInScreen" });
   };
-  const [data, setData] = useState('')
+  const [data, setData] = useState("");
   const [edit, setEdit] = useState(false);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [focusEmail, setFocusEmail] = useState(false);
+  const [focusPhone, setFocusPhone] = useState(false);
+  const [focusJob, setFocusJob] = useState(false);
+  const [focusDescription, setFocusDescription] = useState(false);
+  const scrollViewRef = useRef(ScrollView);
 
   useEffect(() => {
     async function userData() {
@@ -36,44 +41,86 @@ function ProfileScreen(props) {
 
   const onChange = async () => {
     // setLoading(true)
-    const res = await fetch(`http://${MY_IP}:3000/home/updateUser?token=${props.dataUser.token}&description=${data.description}&job=${data.job}`)
+    const res = await fetch(`http://${MY_IP}:3000/home/updateUser?token=${props.dataUser.token}&description=${data.description}&job=${data.job}`);
     if (res) {
-      setEdit(false)
+      setEdit(false);
+      showMessage({
+        message: "Modification enregistrée",
+        type: "success",
+        animationDuration: 500,
+        duration: 3000,
+      });
     } else {
-      console.log('quelque chose a mal tourné !')
+      console.log("quelque chose a mal tourné !");
     }
   };
   if (!data) {
-    return null
+    return null;
   }
 
   return (
     <View style={{ flex: 1 }}>
-      <ScrollView>
+      <ScrollView ref={scrollViewRef}>
         <View style={styles.upperContainer}>
-          <Avatar source={{ uri: data.avatar }} avatarStyle={{ borderRadius: 50, overflow: 'hidden' }} size={"xlarge"} />
-          <Text style={styles.heading}>{data.firstName} {data.lastName}</Text>
+          <Avatar source={{ uri: data.avatar }} avatarStyle={{ borderRadius: 50, overflow: "hidden" }} size={"xlarge"} />
+          <Text style={styles.heading}>
+            {data.firstName} {data.lastName}
+          </Text>
         </View>
-        <View style={styles.detailWrappper}>
+        <View
+          style={focusEmail ? styles.focusInput : styles.detailWrappper}
+          onFocus={() => {
+            setFocusEmail(true);
+          }}
+          onBlur={() => setFocusEmail(false)}>
           <Text style={styles.label}>Email</Text>
-          <Text style={styles.detail}>{data.email}</Text>
+          <TextInput
+            value={data.email}
+            onChangeText={(value) => {
+              // setEdit(true);
+              setData({ ...data, email: value });
+            }}
+            style={styles.detail}
+          />
         </View>
-        <View style={styles.detailWrappper}>
+        <View
+          style={focusPhone ? styles.focusInput : styles.detailWrappper}
+          onFocus={() => {
+            setFocusPhone(true);
+          }}
+          onBlur={() => setFocusPhone(false)}>
           <Text style={styles.label}>Phone</Text>
-          <Text style={styles.detail}>{data.phone}</Text>
+          <TextInput
+            value={data.phone}
+            onChangeText={(value) => {
+              // setEdit(true);
+              setData({ ...data, phone: value });
+            }}
+            style={styles.detail}
+          />
         </View>
-        <View style={styles.detailWrappper}>
+        <View
+          style={focusJob ? styles.focusInput : styles.detailWrappper}
+          onFocus={() => {
+            setFocusJob(true);
+          }}
+          onBlur={() => setFocusJob(false)}>
           <Text style={styles.label}>Job</Text>
           <TextInput
             value={data.job}
             onChangeText={(value) => {
               // setEdit(true);
-              setData({ ...data, job: value })
+              setData({ ...data, job: value });
             }}
             style={styles.detail}
           />
         </View>
-        <View style={styles.detailWrappper}>
+        <View
+          style={focusDescription ? styles.focusInput : styles.detailWrappper}
+          onFocus={() => {
+            setFocusDescription(true);
+          }}
+          onBlur={() => setFocusDescription(false)}>
           <Text style={styles.label}>Description</Text>
           <TextInput
             value={data.description}
@@ -81,20 +128,17 @@ function ProfileScreen(props) {
             numberOfLines={5}
             onChangeText={(value) => {
               // setEdit(true);
-              setData({ ...data, description: value })
+              setData({ ...data, description: value });
             }}
             style={styles.detail}
           />
         </View>
-        <View style={{ marginVertical: 20, alignItems: 'center' }}>
+        <View style={{ marginVertical: 20, alignItems: "center" }}>
           <CreatButton onPress={onChange}>Modifier</CreatButton>
           <View style={{ margin: 5 }}></View>
-          <CreatButton
-            buttonStyle={{ backgroundColor: "#eb4d4b" }}
-            textStyle={{ fontWeight: "bold" }}
-            onPress={() => handleSubmitRemove()}
-          >
-            Déconnexion</CreatButton>
+          <CreatButton buttonStyle={{ backgroundColor: "#eb4d4b" }} textStyle={{ fontWeight: "bold" }} onPress={() => handleSubmitRemove()}>
+            Déconnexion
+          </CreatButton>
         </View>
       </ScrollView>
     </View>
@@ -136,13 +180,19 @@ const styles = StyleSheet.create({
   detail: {
     fontSize: 17,
     marginTop: 5,
+    textAlignVertical: "top",
+  },
+  focusInput: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderBottomColor: "#2C98DA",
+    borderBottomWidth: 2,
+    paddingBottom: 5,
   },
 });
-
 
 function mapStateToProps(state) {
   return { dataUser: state.dataUser };
 }
 
 export default connect(mapStateToProps)(ProfileScreen);
-
