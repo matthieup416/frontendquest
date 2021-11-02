63e75732c00fbccfd1bbc7e36f5b988aa28a229d
Gato18 <g.canonge@gmail.com>
2021-11-02 16:30:27 +0100

    add loader on home/result/listing/explorer

--- screens/ExplorerScreen.js
+++ screens/ExplorerScreen.js
@@ -1,96 +1,88 @@
-import React, { useState, useEffect } from "react"
-import { View, Dimensions, StyleSheet, Image } from "react-native"
-import Icon from "react-native-vector-icons/FontAwesome5"
-import { connect } from "react-redux"
-import { StatusBar } from "expo-status-bar"
-///// WebView permettra d'afficher les miniatures des photos sur la carte via du code HTML (problème des images sur react-native-maps)
-import { WebView } from "react-native-webview"
+import React, { useState, useEffect } from "react";
+import { View, Dimensions, StyleSheet, Image } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { connect } from "react-redux";
+import { StatusBar } from "expo-status-bar";
+// WebView permettra d'afficher les miniatures des photos sur la carte via du code HTML (problème des images sur react-native-maps)
+import { WebView } from "react-native-webview";
+import AnimatedLoader from "react-native-animated-loader";
 
-import MapView, { Marker, Callout } from "react-native-maps"
-import {
-  Card,
-  Text,
-  Button,
-  ListItem,
-  Avatar,
-  Badge,
-} from "react-native-elements"
-import { MY_IP } from "@env" /* Variable environnement */
+import MapView, { Marker, Callout } from "react-native-maps";
+import { Card, Text, Button, ListItem, Avatar, Badge } from "react-native-elements";
+import { MY_IP } from "@env"; /* Variable environnement */
 
-import RNPickerSelect from "react-native-picker-select"
-import { log } from "react-native-reanimated"
+import RNPickerSelect from "react-native-picker-select";
+import { log } from "react-native-reanimated";
 
-let deviceHeight = Dimensions.get("window").height
-let deviceWidth = Dimensions.get("window").width
+let deviceHeight = Dimensions.get("window").height;
+let deviceWidth = Dimensions.get("window").width;
 
 function ExplorerScreen(props) {
-  const [listQuest, setListQuest] = useState([])
-  const [listOffer, setListOffer] = useState([])
-  const [selectedQuest, setSelectedQuest] = useState("")
+  const [listQuest, setListQuest] = useState([]);
+  const [listOffer, setListOffer] = useState([]);
+  const [selectedQuest, setSelectedQuest] = useState("");
+  const [loaderVisible, setLoaderVisible] = useState(true);
   // Sans choix de quete on affiche la carte centrée sur Paris
   const [questCityCoord, setQuestCityCoord] = useState({
     latitude: 48.8588897,
     longitude: 2.320041,
-  })
+  });
   const [questCityRayon, setQuestCityRayon] = useState({
     latitudeDelta: 5.6036,
     longitudeDelta: 5.6036,
-  })
+  });
 
   //Au chargement du composant, on cherche toutes les quêtes de l'utilisateur pour faire le menu select
   useEffect(() => {
     async function listQuest() {
-      const data = await fetch(
-        `http://${MY_IP}:3000/inbox/?token=${props.dataUser.token}`
-      )
-      const body = await data.json()
+      const data = await fetch(`http://${MY_IP}:3000/inbox/?token=${props.dataUser.token}`);
+      const body = await data.json();
       var list = body.listQuest.map((quest) => {
         return {
           value: quest._id,
           label: `${quest.city} - ${quest.min_price}/${quest.max_price}€`,
-        }
-      })
-      setListQuest(list)
-      setSelectedQuest(body.listQuest[0]._id)
+        };
+      });
+      setListQuest(list);
+      setSelectedQuest(body.listQuest[0]._id);
     }
-    listQuest()
-  }, [])
+    listQuest();
+  }, []);
 
   //Quand on click sur une quête, on charge les offres de celle ci.
   useEffect(() => {
     async function results() {
-      const data = await fetch(
-        `http://${MY_IP}:3000/resultsmap/?quest_id=${selectedQuest}&token=${props.dataUser.token}`
-      )
-      const body = await data.json()
+      const data = await fetch(`http://${MY_IP}:3000/resultsmap/?quest_id=${selectedQuest}&token=${props.dataUser.token}`);
+      const body = await data.json();
 
-      setListOffer(body.listOffers)
-      console.log("body", body)
+      setListOffer(body.listOffers);
       setQuestCityCoord({
         latitude: body.cityCoord.latitude,
         longitude: body.cityCoord.longitude,
-      })
-      var newLatitudeDelta =
-        (body.quest.rayon * 0.01) / 1.11 + (15 * 0.01) / 1.11
-      var newLongitudeDelta = newLatitudeDelta
+      });
+      var newLatitudeDelta = (body.quest.rayon * 0.01) / 1.11 + (15 * 0.01) / 1.11;
+      var newLongitudeDelta = newLatitudeDelta;
 
       setQuestCityRayon({
         latitudeDelta: newLatitudeDelta,
         longitudeDelta: newLongitudeDelta,
-      })
+      });
+      setLoaderVisible(false);
     }
-    results()
-  }, [selectedQuest])
+    results();
+  }, [selectedQuest]);
   return (
     <View style={{ flex: 1, marginTop: 25 }}>
+      <AnimatedLoader visible={loaderVisible} overlayColor="rgba(255,255,255,0.75)" animationStyle={styles.lottie} speed={1} source={require("../assets/loader_map.json")}>
+        <Text></Text>
+      </AnimatedLoader>
       <StatusBar backgroundColor={"#2D98DA"} style="light" />
 
       <View
         style={{
           height: 100,
           backgroundColor: "#2D98DA",
-        }}
-      >
+        }}>
         <Text
           style={{
             fontSize: 22,
@@ -99,17 +91,10 @@ function ExplorerScreen(props) {
             width: (deviceWidth * 2) / 3,
             marginTop: 10,
             marginLeft: 10,
-          }}
-        >
+          }}>
           Les dernières offres :
         </Text>
-        <RNPickerSelect
-          onValueChange={(value) => setSelectedQuest(value)}
-          items={listQuest}
-          placeholder={{ label: "Choisir une quête", value: null }}
-          style={pickerSelectStyles}
-          value={selectedQuest}
-        />
+        <RNPickerSelect onValueChange={(value) => setSelectedQuest(value)} items={listQuest} placeholder={{ label: "Choisir une quête", value: null }} style={pickerSelectStyles} value={selectedQuest} />
       </View>
       <MapView
         style={{ flex: 1 }}
@@ -125,13 +110,12 @@ function ExplorerScreen(props) {
           latitudeDelta: questCityRayon.latitudeDelta, // le rayon à afficher à partir du centre (rayon + 5 km)
           longitudeDelta: questCityRayon.longitudeDelta,
         }}
-        zoomEnabled={true}
-      >
+        zoomEnabled={true}>
         {listOffer.map((offer, i) => {
           if (offer.is_pro) {
-            var pro = <Badge status="primary" value="PRO" />
+            var pro = <Badge status="primary" value="PRO" />;
           } else {
-            var pro = <Text></Text>
+            var pro = <Text></Text>;
           }
 
           return (
@@ -142,31 +126,26 @@ function ExplorerScreen(props) {
                 latitude: offer.offers.latitude,
                 longitude: offer.offers.longitude,
               }}
-              opacity={1}
-            >
+              opacity={1}>
               <View style={{ borderRadius: 10 }}>
                 <Callout
                   onPress={() => {
-                    console.log(selectedQuest)
                     props.navigation.navigate("Listing", {
                       questId: selectedQuest,
                       offerId: offer.offers._id,
-                    })
-                  }}
-                >
+                    });
+                  }}>
                   <View
                     style={{
                       flexDirection: "column",
                       width: 250,
                       alignItems: "flex-start",
-                    }}
-                  >
+                    }}>
                     <WebView
                       style={{ width: 250, height: 150 }}
                       source={{
                         html: `<img src='${offer.offers.pictures[0].url}' width="100%"/>`,
-                      }}
-                    ></WebView>
+                      }}></WebView>
 
                     <Text
                       style={{
@@ -175,8 +154,7 @@ function ExplorerScreen(props) {
                         color: "#2D98DA",
                         marginTop: 0,
                         width: 180,
-                      }}
-                    >
+                      }}>
                       {offer.offers.type} {offer.offers.surface} m{"\u00b2"}
                     </Text>
                     <Text
@@ -186,24 +164,28 @@ function ExplorerScreen(props) {
                         color: "#2D98DA",
                         marginTop: 0,
                         width: 180,
-                      }}
-                    >
+                      }}>
                       {offer.offers.city}
                     </Text>
-                    <Text style={{ fontSize: 18, color: "#585858" }}>
-                      {offer.offers.price} €
-                    </Text>
+                    <Text style={{ fontSize: 18, color: "#585858" }}>{offer.offers.price} €</Text>
                     {pro}
                   </View>
                 </Callout>
               </View>
             </Marker>
-          )
+          );
         })}
       </MapView>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  lottie: {
+    width: 250,
+    height: 250,
+  },
+});
 
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
@@ -230,10 +212,10 @@ const pickerSelectStyles = StyleSheet.create({
     backgroundColor: "#2D98DA",
     paddingRight: 30, // to ensure the text is never behind the icon
   },
-})
+});
 
 function mapStateToProps(state) {
-  return { dataUser: state.dataUser }
+  return { dataUser: state.dataUser };
 }
 
-export default connect(mapStateToProps)(ExplorerScreen)
+export default connect(mapStateToProps)(ExplorerScreen);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,173 +1,108 @@
-import React, { useEffect, useState, useCallback } from "react"
-import {
-  View,
-  Text,
-  SafeAreaView,
-  ScrollView,
-  Button,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native"
-import { connect } from "react-redux"
+import React, { useEffect, useState, useCallback } from "react";
+import { View, Text, SafeAreaView, ScrollView, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { connect } from "react-redux";
 
-import { Overlay } from "react-native-elements"
+import { Overlay } from "react-native-elements";
+import AnimatedLoader from "react-native-animated-loader";
+import Header from "../components/header";
+import CreatButton from "../shared/CreatButton";
 
-import Header from "../components/header"
-import CreatButton from "../shared/CreatButton"
-
-import { MY_IP } from "@env" /* Variable environnement */
-import { log } from "react-native-reanimated"
-import { useIsFocused, useFocusEffect } from "@react-navigation/native"
+import { MY_IP } from "@env"; /* Variable environnement */
+import { useIsFocused, useFocusEffect } from "@react-navigation/native";
 
 function HomeScreen(props) {
-  const [data, setData] = useState("")
-  const [overlay, setOverlay] = useState(<></>) // Etat d'overlay
-  const [exclusivity, setExclusivity] = useState(<></>) // Etat d'exclusivité
-  const [quest, setQuest] = useState(0)
-  const [offers, setOffers] = useState(0)
-  const [results, setResults] = useState([])
-  const isFocused = useIsFocused()
+  const [data, setData] = useState("");
+  const [overlay, setOverlay] = useState(<></>); // Etat d'overlay
+  const [exclusivity, setExclusivity] = useState(<></>); // Etat d'exclusivité
+  const [loaderVisible, setLoaderVisible] = useState(true);
+  const [quest, setQuest] = useState(0);
+  const [offers, setOffers] = useState(0);
+  const [results, setResults] = useState([]);
+  const isFocused = useIsFocused();
 
   // Au chargement du composant, on obtient toutes les données de l'utilisateur.
   useEffect(() => {
     async function userData() {
-      const data = await fetch(
-        `http://${MY_IP}:3000/home/userDetail?token=${props.dataUser.token}`
-      )
-      const body = await data.json()
+      const data = await fetch(`http://${MY_IP}:3000/home/userDetail?token=${props.dataUser.token}`);
+      const body = await data.json();
       if (body.result) {
-        // var countresult = [];
-        // for (let i = 0; i < body.user.quests.length; i++) {
-        //   const res = await fetch(`http://${MY_IP}:3000/countresults?token=${props.dataUser.token}&quest_id=${body.user.quests[i]._id}`);
-        //   const count = await res.json();
-        //   countresult.push(count.listOffers);
-        // }
-        // setResults(countresult);
-        setData(body.user)
-        setQuest(body.user.quests.length)
+        setData(body.user);
+        setQuest(body.user.quests.length);
         props.addUser({
           token: body.user.token,
           firstName: body.user.firstName,
           avatar: body.user.avatar,
-        })
+        });
       } else {
-        console.log("error")
+        console.log("error");
       }
     }
-    userData()
-  }, [])
+    userData();
+  }, []);
 
   //Relance la fonction useData à chaque fois que l'écran est focus
   useFocusEffect(
     useCallback(() => {
-      userData()
+      userData();
     }, [])
-  )
+  );
 
-  const viewexclusivity = () => {
+  const viewExclusivity = () => {
     setExclusivity(
-      <Overlay
-        isVisible={true}
-        overlayStyle={{ backgroundColor: "#F8F7FF" }}
-        onBackdropPress={() => setExclusivity(<></>)}
-      >
+      <Overlay isVisible={true} overlayStyle={{ backgroundColor: "#F8F7FF" }} onBackdropPress={() => setExclusivity(<></>)}>
         <Text style={styles.title}>Super exclusivité</Text>
       </Overlay>
-    )
-  }
+    );
+  };
 
   // Fonction de l'overlay pour le rendre visible ou non.
   const toggleOverlay = (item) => {
     setOverlay(
-      <Overlay
-        isVisible={true}
-        overlayStyle={{ backgroundColor: "#F8F7FF" }}
-        onBackdropPress={() => setOverlay(<></>)}
-      >
+      <Overlay isVisible={true} overlayStyle={{ backgroundColor: "#F8F7FF" }} onBackdropPress={() => setOverlay(<></>)}>
         <View style={{ padding: 15 }}>
           <Text style={styles.title}>Vos options de quêtes!</Text>
-          <Text style={styles.overText}>
-            Prix minimum:{" "}
-            {item.min_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
-          </Text>
-          <Text style={styles.overText}>
-            Prix maximum:{" "}
-            {item.max_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
-          </Text>
-          <Text style={styles.overText}>
-            Surface maximum: {item.max_surface}
-          </Text>
-          <Text style={styles.overText}>
-            Surface minimum: {item.min_surface}
-          </Text>
-          <Text style={styles.overText}>
-            Surface extérieur: {item.outdoor_surface}
-          </Text>
+          <Text style={styles.overText}>Prix minimum: {item.min_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</Text>
+          <Text style={styles.overText}>Prix maximum: {item.max_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</Text>
+          <Text style={styles.overText}>Surface maximum: {item.max_surface}</Text>
+          <Text style={styles.overText}>Surface minimum: {item.min_surface}</Text>
+          <Text style={styles.overText}>Surface extérieur: {item.outdoor_surface}</Text>
           <Text style={styles.overText}>Pieces maximum: {item.pieces_max}</Text>
           <Text style={styles.overText}>Pieces minimum: {item.pieces_min}</Text>
-          <Text style={styles.overText}>
-            Ascenseur: {item.elevator ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Parking: {item.parking ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Ancien: {item.is_old ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Neuf: {item.is_new ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Fibre optique: {item.fiber_optics ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Piscine: {item.pool ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Balcon: {item.balcony ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Terrasse: {item.terrace ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Date de création:{" "}
-            {item.created && item.created.split("T")[0].replace(/-/g, "/")}
-          </Text>
-          <Text style={styles.overText}>
-            Date du marché: {item.market_date ? "oui" : "non"}
-          </Text>
-          <Text style={styles.overText}>
-            Disponible aux pro: {item.open_to_pro ? "oui" : "non"}
-          </Text>
+          <Text style={styles.overText}>Ascenseur: {item.elevator ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Parking: {item.parking ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Ancien: {item.is_old ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Neuf: {item.is_new ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Fibre optique: {item.fiber_optics ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Piscine: {item.pool ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Balcon: {item.balcony ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Terrasse: {item.terrace ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Date de création: {item.created && item.created.split("T")[0].replace(/-/g, "/")}</Text>
+          <Text style={styles.overText}>Date du marché: {item.market_date ? "oui" : "non"}</Text>
+          <Text style={styles.overText}>Disponible aux pro: {item.open_to_pro ? "oui" : "non"}</Text>
         </View>
       </Overlay>
-    )
-  }
+    );
+  };
 
   // Réutilisation de la fonction pour le refresh de la page.
   async function userData() {
-    const data = await fetch(
-      `http://${MY_IP}:3000/home/userDetail?token=${props.dataUser.token}`
-    )
-    const body = await data.json()
+    const data = await fetch(`http://${MY_IP}:3000/home/userDetail?token=${props.dataUser.token}`);
+    const body = await data.json();
     if (body.result) {
-      var countresult = []
+      var countresult = [];
       for (let i = 0; i < body.user.quests.length; i++) {
-        const res = await fetch(
-          `http://${MY_IP}:3000/countresults?token=${props.dataUser.token}&quest_id=${body.user.quests[i]._id}`
-        )
-        const count = await res.json()
-        console.log(count.listOffers)
-        countresult.push(count.listOffers)
+        const res = await fetch(`http://${MY_IP}:3000/countresults?token=${props.dataUser.token}&quest_id=${body.user.quests[i]._id}`);
+        const count = await res.json();
+        countresult.push(count.listOffers);
       }
-      setResults(countresult)
-      console.log("countresult", countresult)
-      setData(body.user)
-      setQuest(body.user.quests.length)
-      setOffers(body.user.offers.length)
-      props.addUser
+      setResults(countresult);
+      setData(body.user);
+      setQuest(body.user.quests.length);
+      setOffers(body.user.offers.length);
+      props.addUser;
+      setLoaderVisible(false);
     } else {
-      console.log("error")
+      console.log("error");
     }
   }
 
@@ -176,20 +111,19 @@ function HomeScreen(props) {
     props.navigation.navigate("Results", {
       screen: "ResultsScreen",
       questId: id,
-    })
-  }
+    });
+  };
 
   if (isFocused) {
     return (
       <SafeAreaView>
         {overlay}
         {exclusivity}
+        <AnimatedLoader visible={loaderVisible} overlayColor="rgba(255,255,255,0.75)" animationStyle={styles.lottie} speed={1} source={require("../assets/loader.json")}>
+          <Text></Text>
+        </AnimatedLoader>
         <ScrollView>
-          <Header
-            onRefresh={userData}
-            title={data.firstName}
-            image={data.avatar}
-          />
+          <Header onRefresh={userData} title={data.firstName} image={data.avatar} />
           <Text
             style={{
               textAlign: "center",
@@ -198,8 +132,7 @@ function HomeScreen(props) {
               color: "#2C98DA",
               fontWeight: "bold",
             }}
-            onPress={() => viewExclusivity()}
-          >
+            onPress={() => viewExclusivity()}>
             Vos {quest} quêtes en cours
           </Text>
           <TouchableOpacity
@@ -207,9 +140,8 @@ function HomeScreen(props) {
             onPress={() => {
               props.navigation.navigate("AddQuest", {
                 screen: "AddQuestScreen",
-              })
-            }}
-          >
+              });
+            }}>
             <Text style={styles.buttonText}>Lancez une quête</Text>
           </TouchableOpacity>
           {data.quests?.map((item, i) => {
@@ -223,14 +155,12 @@ function HomeScreen(props) {
                   marginVertical: 5,
                   borderRadius: 10,
                   marginHorizontal: 10,
-                }}
-              >
+                }}>
                 <View
                   style={{
                     flexDirection: "row",
                     justifyContent: "space-between",
-                  }}
-                >
+                  }}>
                   <Text style={styles.text}>{item.type}</Text>
                   <Text style={styles.text}>{item.city}</Text>
                   <Text style={styles.text}>Rayon {item.rayon} </Text>
@@ -240,52 +170,35 @@ function HomeScreen(props) {
                     marginVertical: 20,
                     flexDirection: "row",
                     justifyContent: "space-between",
-                  }}
-                >
-                  <Text style={styles.text}>
-                    prix min :{" "}
-                    {item.min_price
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
-                    €
-                  </Text>
-                  <Text style={styles.text}>
-                    prix max :{" "}
-                    {item.max_price
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
-                    €
-                  </Text>
+                  }}>
+                  <Text style={styles.text}>prix min : {item.min_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}€</Text>
+                  <Text style={styles.text}>prix max : {item.max_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}€</Text>
                 </View>
                 <View
                   style={{
                     flexDirection: "row",
                     justifyContent: "space-between",
-                  }}
-                >
-                  <CreatButton onPress={() => toggleOverlay(item)}>
-                    Détails
-                  </CreatButton>
+                  }}>
+                  <CreatButton onPress={() => toggleOverlay(item)}>Détails</CreatButton>
                   <CreatButton
                     result={results[i]}
                     onPress={() => {
-                      handleResult(item._id)
+                      handleResult(item._id);
                     }}
-                    buttonStyle={{ backgroundColor: "rgba(251, 197, 49, 1)" }}
-                  >
+                    buttonStyle={{ backgroundColor: "rgba(251, 197, 49, 1)" }}>
                     <Text style={[styles.textButton, styles[results[i]]]}>
                       {results[i]} {results[i] > 1 ? "RÉSULTATS" : "RÉSULTAT"}
                     </Text>
                   </CreatButton>
                 </View>
               </View>
-            )
+            );
           })}
         </ScrollView>
       </SafeAreaView>
-    )
+    );
   } else {
-    return <View style={{ flex: 1 }}></View>
+    return <View style={{ flex: 1 }}></View>;
   }
 }
 
@@ -318,18 +231,22 @@ const styles = StyleSheet.create({
   0: {
     color: "rgba(0, 0, 0, 0.2)",
   },
-})
+  lottie: {
+    width: 250,
+    height: 250,
+  },
+});
 
 function mapStateToProps(state) {
-  return { dataUser: state.dataUser }
+  return { dataUser: state.dataUser };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     addUser: function (dataUser) {
-      dispatch({ type: "addUser", dataUser: dataUser })
+      dispatch({ type: "addUser", dataUser: dataUser });
     },
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
